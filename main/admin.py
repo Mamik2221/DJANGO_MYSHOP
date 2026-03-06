86587ad86ff0b946f67081aee0532baed4528758
Abdul-Malik Dzhukaev <mamik.shali@gmail.com>
2026-03-06 15:59:28 +0300

    Добавил админку

---
+++ main/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug" : ("name",) }
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    list_display = ["name", "category", "price", "available",
+                    "created", "update"]
+    list_filter = ["available", "created", "update", "category"]
+    list_editable = ["price", "available"]
+    prepopulated_fields = {"slug" : ("name",)}
+
+# Register your models here.
